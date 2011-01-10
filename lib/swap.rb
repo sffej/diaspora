71ac92d44eed90822c658dcd2e5f6e7eec32c85f
root <root@diasp.org>
2011-01-10 11:23:41 -0800

    key

--- lib/swap.rb
+++ lib/swap.rb
@@ -12,6 +12,7 @@ require 'rubygems'
       else
         #do
         apikey = AppConfig[:yourlsapi]
+Rails.logger.info("message=#{apikey}")
         newurl = Net::HTTP.get(URI.parse("http://boun.cc/yourls-api.php?action=shorturl&url=#{$2}://#{$3}&signature=#{apikey}&format=simple"))
         res = %{#{newurl}}
         res.gsub!(/(\*|_)/) { |m| "\\#{$1}" }
