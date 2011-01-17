dc28bd1b87a255a4939ea2271c4836cd331589c0
David Morley <dia@z8s.com>
2011-01-16 18:42:54 -0800

    remove all my stuff i moved to its own

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -98,51 +98,5 @@ class UsersController < ApplicationController
     send_data( File.open(tar_path).read, :filename => "#{current_user.id}.tar" )
   end
 
-  require File.join(Rails.root, 'lib/forwardmail')
-
-  def forwardemail
-     user = User.find_by_username(params[:username])
-render :layout => false
-return
-
-  end
-
-  def forwardemailon
-     forward = Morley::Forwardmailon::forward(current_user.username,current_user.email,1)
-     results = forward.split(":")
-        if results[0] == "Success" 
-         render :text => results[1], :status => 200 
-        else
-         render :text => results[1], :status => 200
-	end
-  end
-
-  def forwardemailoff
-     forward = Morley::Forwardmailon::forward(current_user.username,current_user.email,0)
-     results = forward.split(":")
-	if results[0] == "Success" 
-         render :text => results[1], :status => 200
-	else
-         render :text => results[1], :status => 200
-	end
-  end
-
-  def help
-render :layout => false
-return
-
-  end
-
-  def status
-render :layout => false
-return
-  end
-
-  def fb
-        @user     = current_user.diaspora_handle
-render :layout => false
-return
-
-  end
 end
 
