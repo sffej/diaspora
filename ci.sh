439a5b07497998225ed3c5182f81d48c3bb80ddd
David Morley <dia@z8s.com>
2011-01-30 11:28:15 -0800

    lets get my ci back online eh?

--- ci.sh
+++ ci.sh
@@ -4,8 +4,8 @@ echo "**************************************************************************
 echo "*                                  ruby 1.8.7 REE build                                         *" &&
 echo "*************************************************************************************************" &&
 echo "" &&
-rm -f Gemfile.lock &&
-source /usr/local/rvm/scripts/rvm &&
-rvm use ree@diaspora --create &&
+#rm -f Gemfile.lock &&
+#source /usr/local/rvm/scripts/rvm &&
+#rvm use ree@diaspora --create &&
 bundle install &&
-bundle exec rake cruise
+bundle exec rake spec
