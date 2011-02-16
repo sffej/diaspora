aaa3f874f826aef69a1f08f0805f3c55ecb01f46
David Morley <dia@z8s.com>
2011-02-16 09:10:29 -0800

    move the thingy

--- public/javascripts/diasporg.js
+++ public/javascripts/diasporg.js
@@ -2,21 +2,8 @@ $(document).ready(function() {
 
    $('#status_message_fake_message').jqEasyCounter({'maxChars': 1000,'maxCharsWarning': 140});
    $("textarea[id*='comment_text_on_']").jqEasyCounter({'maxChars': 1000,'maxCharsWarning': 140});
-   $('#shorten').qtip({
-      content: 'Shorten your URL by adding an extra / in it!<br>E.g. http:<b>///</b>www.com or https:<b>///</b>www.com',
-      position: {
-         my: 'top left',
-         target: 'mouse',
-         viewport: $(window), // Keep it on-screen at all times if possible
-         adjust: {
-            x: 10,  y: 10
-         }
-      },
-      hide: {
-         fixed: true // Helps to prevent the tooltip from hiding ocassionally when tracking!
-      },
-      style: 'ui-tooltip-dark'
-   });
+   $('#shorten').qtip({content: 'Shorten your URL by adding an extra / in it!<br>E.g. http:<b>///</b>www.com or https:<b>///</b>www.com',
+      position: {my: 'top left', target: 'mouse', viewport: $(window),adjust: {x: 10,  y: 10}},hide: {fixed: true},style: 'ui-tooltip-dark'});
 
    $(".expand").live('mouseover', function() {
    $(this).qtip({
@@ -67,10 +54,28 @@ $(document).ready(function() {
       },
       style: 'ui-tooltip-dark'
    });
-});
-
-   $('img.avatar').each(function() {
-   $(this).attr('title', this.alt+'\'s pod is '+ this.src.split(/\/+/g)[1].replace(new RegExp(/^www\./i),"").replace(new RegExp(/.s3.amazonaws.com/i),""));
    });
-});
 
+   $(".avatar").live('mouseover', function() {
+   $(this).qtip({
+      content: this.alt+'\'s pod is '+ this.src.split(/\/+/g)[1].replace(new RegExp(/^www\./i),"").replace(new RegExp(/.s3.amazonaws.com/i),""),
+      show: {
+         ready: true // Needed to make it show on first mouseover event
+      },
+      position: {
+         my: 'bottom left',
+         target: $(this),
+         viewport: $(window), // Keep it on-screen at all times if possible
+         adjust: {
+            x: -10,  y: -40
+         }
+      },
+      hide: {
+         fixed: true // Helps to prevent the tooltip from hiding ocassionally when tracking!
+      },
+      style: 'ui-tooltip-dark'
+   });
+   });
+
+
+});
