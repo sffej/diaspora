146c7b1a0cac9035a1466486a59ac825eafdf747
David Morley <dia@z8s.com>
2011-03-01 10:27:26 -0800

    is ci not test?

--- ci.sh
+++ ci.sh
@@ -5,5 +5,5 @@ echo "*                                  ruby 1.8.7 REE build                   
 echo "*************************************************************************************************" &&
 echo "" &&
 bundle install &&
-RAILS_ENV=test rake db:migrate
+RAILS_ENV=development rake db:migrate
 bundle exec rake spec SPEC_OPTS="--tag ~performance"
