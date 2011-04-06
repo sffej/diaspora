25a3cf705ea799332b30d52e1f68e5533e5f78ea
David Morley <dia@z8s.com>
2011-04-06 16:49:52 -0700

    fix url expander and write my own thingy

--- public/javascripts/diasporg.js
+++ public/javascripts/diasporg.js
@@ -1,18 +1,14 @@
-$(document).ready(function()
-{
-$('.expand').tipsy({live: true,html:true,fallback:'Getting Data, rehover on URL to refresh!'});
-   $('.expand').live('mouseover',function()
-   {
-     $.ajax({
-     url: "/shorten/show/?url="+this.href,
-       success: function(data) {
-       $(this).addClass("done");
-       $(this).attr('title',data);
-       }
-     });
-
-   });
-
+$('.expanded').tipsy({live: true,html:true});
+$('.expand').live('mouseenter', function() {
+  $(this).append("<div class='urlnote'>URL Expanded re-hover to see data</div>")
+  var $self = $(this)
+    $.ajax({
+      url: '/shorten/show?url='+this.href,
+        success: function(data) {
+        $self.addClass("expanded").removeClass('expand').attr('title',data).css('color','green');
+        }
+    });
+$('.urlnote').hide(2500);
 });
 
 
