543af588683dca9168218d1ad2b45602f5f412c0
David Morley <support@diasp.org>
2011-05-21 09:22:25 -0700

    undo

--- db/migrate/20110405171412_contact_remove_pending_add_sharing_and_receiving.rb
+++ db/migrate/20110405171412_contact_remove_pending_add_sharing_and_receiving.rb
@@ -1,33 +1,33 @@
 class ContactRemovePendingAddSharingAndReceiving < ActiveRecord::Migration
   def self.up
-#    add_column :contacts, :sharing, :boolean, :default => false, :null => false
-#    add_column :contacts, :receiving, :boolean, :default => false, :null => false
+    add_column :contacts, :sharing, :boolean, :default => false, :null => false
+    add_column :contacts, :receiving, :boolean, :default => false, :null => false
 
-#    execute( <<SQL
-#      UPDATE contacts
-#        SET contacts.sharing = true, contacts.receiving = true
-#          WHERE contacts.pending = false
-#SQL
-#)
+    execute( <<SQL
+      UPDATE contacts
+        SET contacts.sharing = true, contacts.receiving = true
+          WHERE contacts.pending = false
+SQL
+)
 
-#    execute( <<SQL
-#      DELETE user_preferences.* FROM user_preferences
-#        WHERE user_preferences.email_type = 'request_acceptance'
-#          OR user_preferences.email_type = 'request_received'
-#SQL
-#)
+    execute( <<SQL
+      DELETE user_preferences.* FROM user_preferences
+        WHERE user_preferences.email_type = 'request_acceptance'
+          OR user_preferences.email_type = 'request_received'
+SQL
+)
 
-#    remove_foreign_key "contacts", "people"
-#    remove_index :contacts, [:person_id, :pending]
-#    remove_index :contacts, [:user_id, :pending]
+    remove_foreign_key "contacts", "people"
+    remove_index :contacts, [:person_id, :pending]
+    remove_index :contacts, [:user_id, :pending]
 
-#    add_index :contacts, :person_id
-#    add_foreign_key "contacts", "people", :name => "contacts_person_id_fk", :dependent => :delete
+    add_index :contacts, :person_id
+    add_foreign_key "contacts", "people", :name => "contacts_person_id_fk", :dependent => :delete
 
-#    remove_column :contacts, :pending
-#
-#    remove_foreign_key :aspect_memberships, :aspects
-#    add_foreign_key :aspect_memberships, :aspects, :dependent => :delete
+    remove_column :contacts, :pending
+
+    remove_foreign_key :aspect_memberships, :aspects
+   add_foreign_key :aspect_memberships, :aspects, :dependent => :delete
   end
 
   def self.down
