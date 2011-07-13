f985e583f2b7d11ef1cf5bf32ee98985930c8874
David Morley <support@diasp.org>
2011-07-13 09:56:27 -0700

    fix ci

--- ci.sh
+++ ci.sh
@@ -5,7 +5,7 @@ echo "*     t                             ruby 1.8.7     build                  
 echo "*************************************************************************************************" &&
 echo "" &&
 bundle install &&
-#RAILS_ENV=development rake db:migrate
+RAILS_ENV=development rake db:migrate
 #bundle exec rake spec SPEC_OPTS="--tag ~performance"
 #cp config/application.yml.example config/application.yml &&
 #bundle exec rake cruise
