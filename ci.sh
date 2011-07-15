12d7f389416168dcb451b3eedd83517a1928a954
David Morley <support@diasp.org>
2011-07-14 21:19:12 -0700

    ci push

--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ echo "**************************************************************************
 echo "" &&
 bundle install &&
 RAILS_ENV=development rake db:migrate
-#bundle exec rake spec SPEC_OPTS="--tag ~performance"
+#hbundle exec rake spec SPEC_OPTS="--tag ~performance"
 #cp config/application.yml.example config/application.yml &&
 #bundle exec rake cruise
 bundle exec rake spec SPEC_OPTS="--tag ~performance"
