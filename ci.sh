54412db0643a8ee05067b8ae58436a66253c6e18
David Morley <support@diasp.org>
2011-07-24 09:33:51 -0700

    moar ci pain

--- ci.sh
+++ ci.sh
@@ -6,6 +6,6 @@ echo "**************************************************************************
 echo "" &&
 bundle install &&
 RAILS_ENV=development rake db:migrate
-#cp config/application.yml.example config/application.yml &&
-cp /root/spec_helper.rb spec/spec_helper.rb
+cp config/application.yml.example config/application.yml &&
+#cp /root/spec_helper.rb spec/spec_helper.rb
 bundle exec rake spec SPEC_OPTS="--tag ~performance"
