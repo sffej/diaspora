e6b25324a9c8713d096777693b14e04e60e6e79b
David Morley <davidmorley@diasp.org>
2011-09-09 13:23:50 -0500

    try CI=true bundle exec rake cruise

--- ci.sh
+++ ci.sh
@@ -7,6 +7,6 @@ echo "" &&
 bundle install &&
 RAILS_ENV=development rake db:migrate
 cp config/application.yml.example config/application.yml &&
-#cp /root/spec_helper.rb spec/spec_helper.rb
-CI=true bundle exec rake spec SPEC_OPTS="--tag ~performance"
+#CI=true bundle exec rake spec SPEC_OPTS="--tag ~performance"
 #bundle exec rake spec
+CI=true bundle exec rake cruise SPEC_OPTS="--tag ~performance"
