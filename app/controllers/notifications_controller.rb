95227c4ff9ba01528e3d88353c657b6276bc0fce
David Morley <davidmorley@diasp.org>
2011-09-18 16:22:17 -0500

    merch link

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,7 +7,6 @@ class NotificationsController < VannaController
 
   include ActionController::MobileFu
   has_mobile_fu
-
   def update(opts=params)
     note = Notification.where(:recipient_id => current_user.id, :id => opts[:id]).first
     if note
