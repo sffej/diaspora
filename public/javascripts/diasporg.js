90b789dd08b4c95fd6ccea5deac856ca27fc92ad
David Morley <davidmorley@diasp.org>
2011-10-08 22:30:53 -0500

    lil youtube hack for now, not knowing was driving me nuts

--- public/javascripts/diasporg.js
+++ public/javascripts/diasporg.js
@@ -1,11 +1,30 @@
-$("a[href*='dia.so'][class!='expanded'],a[href*='t.co'][class!='expanded'],a[href*='bit.ly'][class!='expanded'],a[href*='goo.gl'][class!='expanded']").live('mouseenter', function() {
+$("a[href*='http://dia.so'][class!='expanded'],a[href*='http://t.co'][class!='expanded'],a[href*='http://bit.ly'][class!='expanded'],a[href*='http://goo.gl'][class!='expanded']").live('mouseenter', function() {
+$(this).css('cursor','wait');
   var $self = $(this)
     $.ajax({
       url: '/shorten/show?url='+this.href,
         success: function(data) {
         $self.addClass("expanded").append("<span class='via'> "+data+" </span>");
+        $self.css('cursor','alias');
         }
     });
 });
 
+
+$("a[href*='youtube.com'],a[href*='youtu.be']").live({
+      mouseenter: function() { 
+        var $self = $(this);
+        if (this.href.indexOf("youtu.be") > 0) {
+       	   id =	this.href.split("/");
+           check = "http://www.youtube.com/watch?v="+id[3];
+        } else {
+           check = this.href
+        }
+        url = $.jYoutube(check);
+          $self.append("<span id='youtubeimg'><br><img src="+url+"></span>");
+      },
+      mouseleave: function() { 
+          $("span#youtubeimg").delay(633).remove();
+      }
+});
 
