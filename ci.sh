56106b51a69f27acf826c8267db18872289c5ae3
david <david@lappy.(none)>
2012-01-07 16:16:08 -0800

    test dev on lappy

--- ci.sh
+++ ci.sh
@@ -4,3 +4,4 @@ RAILS_ENV=development rake db:migrate
 cp config/application.yml.example config/application.yml &&
 bundle exec rake db:test:prepare
 bundle exec rake spec SPEC_OPTS="--tag ~performance"
+echo "test"
