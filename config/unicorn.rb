927192093f8aa620e292cdd9708f579b0c0b5a8f
David Morley <support@diasp.org>
2012-08-09 07:57:55 -0500

    ug

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,19 @@
-rails_env = 'production'
+rails_env = ENV['RAILS_ENV'] || 'development'
 
 # Enable and set these to run the worker as a different user/group
 #user  = 'diaspora'
 #group = 'diaspora'
-stderr_path "/home/dmm/diaspora/log/unicorn.stderr.log"
-stdout_path "/home/dmm/diaspora/log/unicorn.stdout.log"
 
-worker_processes 1
+worker_processes 5
 
 ## Load the app before spawning workers
-preload_app false
+preload_app true
 
 # How long to wait before killing an unresponsive worker
-timeout 60
+timeout 30
 
 pid '/home/dmm/diaspora/tmp/pids/unicorn.pid'
 listen '/home/dmm/run/diaspora.sock', :backlog => 2048
-#listen '192.168.1.2:8080', :backlog => 2048
 
 # Ruby Enterprise Feature
 if GC.respond_to?(:copy_on_write_friendly=)
@@ -26,14 +23,14 @@ end
 
 before_fork do |server, worker|
   # If using preload_app, enable this line
-  #ActiveRecord::Base.connection.disconnect!
+  ActiveRecord::Base.connection.disconnect!
 
   # disconnect redis if in use
-#  if !AppConfig.single_process_mode?
-#    Resque.redis.client.disconnect
-#  end
+  if !AppConfig.single_process_mode?
+    Resque.redis.client.disconnect
+  end
 
-  old_pid = '/home/dmm/diaspora/tmp/pids/diaspora.pid.oldbin'
+  old_pid = '/var/run/diaspora/diaspora.pid.oldbin'
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
@@ -46,17 +43,17 @@ end
 
 after_fork do |server, worker|
   # If using preload_app, enable this line
-  #ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection
 
   # copy pasta from resque.rb because i'm a bad person
-#  if !AppConfig.single_process_mode?
-#    if redis_to_go = ENV["REDISTOGO_URL"]
-#      uri = URI.parse(redis_to_go)
-##      Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-#    elsif AppConfig[:redis_url]
-#      Resque.redis = Redis.new(:host => AppConfig[:redis_url], :port => 6379)
-#    end
-#  end
+  if !AppConfig.single_process_mode?
+    if redis_to_go = ENV["REDISTOGO_URL"]
+      uri = URI.parse(redis_to_go)
+      Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    elsif AppConfig[:redis_url]
+      Resque.redis = Redis.new(:host => AppConfig[:redis_url], :port => 6379)
+    end
+  end
 
   # Enable this line to have the workers run as different user/group
   #worker.user(user, group)
