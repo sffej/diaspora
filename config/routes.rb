eb4ce562ac41c527761701a59a17cd3fc2d9da74
David Morley <support@diasp.org>
2013-07-03 20:10:52 -0500

    more cleanup, this still is not going to fix them failing specs, hrm

--- config/routes.rb
+++ config/routes.rb
@@ -180,9 +180,6 @@ Diaspora::Application.routes.draw do
   end
   get '/u/:username' => 'people#show', :as => 'user_profile'
   get '/u/:username/profile_photo' => 'users#user_photo'
-  constraints(Subdomain) do
-    match '/' => 'users#user_photo'
-  end
 
   # Federation
 
