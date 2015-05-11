25084dbe1e4770bddb415c3cfc55778dea3a2c40
David Morley <davidmorley@diasp.org>
2015-05-11 09:29:23 -0700

    more old junk

--- public/testing.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-//dia.so
-$("#status_message_fake_text").blur(function() {
-var text = $('#status_message_fake_text').val();
-  if (text.match(/:\/\//ig)) {
-     Diaspora.page.flashMessages.render({success: true, notice: "Protip: Auto-Shorten a url with dia.so by adding a extra / to it in your post, eg http:///domain.com"});
-  }
-});
